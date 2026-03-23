76e3f515cb9ed8aae458972e08b41020939ec481
swapnika0109 <swapnika0109@gmail.com>
2026-03-23 07:42:28 +0530

    initialized app configurations with global routing and error handling

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 const express = require('express')
 const routes = require('./routes/index')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use('/api', routes)
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).json({ message: 'Internal server error' })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server started running on port ${PORT}`)
+})
+
+module.exports = app
