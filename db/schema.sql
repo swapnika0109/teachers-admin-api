4a38636678fd01a3f1208a65c75c73a8dcf9943d
swapnika0109 <swapnika0109@gmail.com>
2026-03-23 07:40:16 +0530

    Added db schema, initialized app configurations

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS teacher_admin;
+USE teacher_admin;
+
+CREATE TABLE IF NOT EXISTS teachers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE
+    is_suspended BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS teacher_students(
+    teacher_id INT NOT NULL,
+    student_id INT NOT NULL,
+    PRIMARY KEY(teacher_id, student_id),
+    FOREIGN KEY(teacher_id) REFERENCES teachers(id),
+    FOREIGN KEY(student_id) REFERENCES students(id)
+);
